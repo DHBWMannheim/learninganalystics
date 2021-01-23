9396016355017c88ec891af9fa80877d7da15c21
Matthias Vonend <matthias.vonend@outlook.de>
2021-01-23 19:34:24 +0100

    init

---
+++ src/app/@core/core.module.ts
@@ -0,0 +1,102 @@
+import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NbAuthModule, NbDummyAuthStrategy } from '@nebular/auth';
+import { NbSecurityModule, NbRoleProvider } from '@nebular/security';
+import { of as observableOf } from 'rxjs';
+
+import { throwIfAlreadyLoaded } from './module-import-guard';
+import { UserData } from './data/users';
+import { UserService } from './mock/users.service';
+import { MockDataModule } from './mock/mock-data.module';
+import { NbFirebasePasswordStrategy, NbFirebaseGoogleStrategy, NbFirebaseAuthModule} from '@nebular/firebase-auth';
+
+
+const socialLinks = [
+  {
+    url: 'https://twitter.com/akveo_inc',
+    target: '_blank',
+    icon: 'google',
+  },
+];
+
+const DATA_SERVICES = [
+  { provide: UserData, useClass: UserService },
+];
+
+export class NbSimpleRoleProvider extends NbRoleProvider {
+  getRole() {
+    // here you could provide any role based on any auth flow
+    return observableOf('guest');
+  }
+}
+
+export const NB_CORE_PROVIDERS = [
+  ...MockDataModule.forRoot().providers,
+  ...DATA_SERVICES,
+  ...NbAuthModule.forRoot({
+
+    strategies: [
+      NbDummyAuthStrategy.setup({
+        name: 'email',
+        delay: 3000,
+      }),
+      NbFirebasePasswordStrategy.setup({
+        name: 'password',
+      }),
+      NbFirebaseGoogleStrategy.setup({
+        name: 'google'
+      })
+    ],
+    forms: {
+      login: {
+        socialLinks: socialLinks,
+      },
+      register: {
+        socialLinks: socialLinks,
+      },
+    },
+  }).providers,
+
+  NbSecurityModule.forRoot({
+    accessControl: {
+      guest: {
+        view: '*',
+      },
+      user: {
+        parent: 'guest',
+        create: '*',
+        edit: '*',
+        remove: '*',
+      },
+    },
+  }).providers,
+
+  {
+    provide: NbRoleProvider, useClass: NbSimpleRoleProvider,
+  },
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+  ],
+  exports: [
+    NbAuthModule,
+    NbFirebaseAuthModule
+  ],
+  declarations: [],
+})
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    throwIfAlreadyLoaded(parentModule, 'CoreModule');
+  }
+
+  static forRoot(): ModuleWithProviders<CoreModule> {
+    return {
+      ngModule: CoreModule,
+      providers: [
+        ...NB_CORE_PROVIDERS,
+      ],
+    };
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,22 @@
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyChlnylXnNoF8CNnTNzP3fokZM7-Rujc5w",
+    authDomain: "dhbwlearning.firebaseapp.com",
+    databaseURL: "https://dhbwlearning.firebaseio.com",
+    projectId: "dhbwlearning",
+    storageBucket: "dhbwlearning.appspot.com",
+    messagingSenderId: "324630061832",
+    appId: "1:324630061832:web:cbbf187d13cc2ec645c2bd",
+  },
+};
