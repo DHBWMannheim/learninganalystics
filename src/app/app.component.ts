e31834f33006595a986bc7fd6bade49d8b91432e
Matthias Vonend <matthias.vonend@outlook.de>
2021-02-15 08:40:15 +0100

    fix service worker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
-/**
- * @license
- * Copyright Akveo. All Rights Reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- */
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'ngx-app',
   template: '<router-outlet></router-outlet>',
 })
 export class AppComponent {
-
+  constructor(private swUpdate: SwUpdate) {}
+  ngOninit() {
+    this.swUpdate.available.subscribe((event) => {
+      console.log(
+        'A newer version is now available. Refresh the page now to update the cache',
+      );
+      location.reload();
+    });
+    this.swUpdate.checkForUpdate();
+  }
 }
